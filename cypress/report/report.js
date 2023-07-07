73bf5be01a4499067323316117676e9667d4afcc
valeria Gerez <vgerez@spins.com>
2023-07-07 15:31:28 -0300

    Added screenshots on report

--- cypress/report/report.js
+++ cypress/report/report.js
@@ -1,20 +1,20 @@
-const fs = require('fs-extra');
-const path = require('path');
-const cucumberHTMLReporter = require('cucumber-html-reporter');
+const fs = require('fs-extra')
+const path = require('path')
+const cucumberHTMLReporter = require('cucumber-html-reporter')
 
 const execute = (rootDirectory) => {
   try {
-    const reportDir = path.normalize(`${rootDirectory}/../report/cucumber_report/html`);
-    const jsonReportDir = path.normalize(`${rootDirectory}/../report/cucumber_report`);
-    const outputReportFilePath = path.normalize(`${rootDirectory}/../report/cucumber_report/html/cucumber_report.html`);
-    const screenshotsDir = path.normalize(`${rootDirectory}/../screenshots/`);
+    const reportDir = path.normalize(`${rootDirectory}/../report/cucumber_report/html`)
+    const jsonReportDir = path.normalize(`${rootDirectory}/../report/cucumber_report`)
+    const outputReportFilePath = path.normalize(`${rootDirectory}/../report/cucumber_report/html/cucumber_report.html`)
+    const screenshotsDir = path.normalize(`${rootDirectory}/../screenshots/`)
 
     if (fs.existsSync(reportDir)) {
       fs.removeSync(reportDir);
     }
     fs.mkdirSync(reportDir);
 
-    console.log('Configuring report ...');
+    console.log('Configuring report ...')
     const reporter = cucumberHTMLReporter;
 
     const options = {
@@ -28,11 +28,11 @@ const execute = (rootDirectory) => {
       screenshotsDirectory: screenshotsDir
     };
 
-    console.log('Generating report ...');
-    reporter.generate(options);
+    console.log('Generating report ...')
+    reporter.generate(options)
 
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
 };
 
